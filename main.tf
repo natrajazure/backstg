4f667859c7e0c5f92042a6f15db7780c672d3e48
natrajazure <natrajv.mecse@gmail.com>
2024-07-12 10:06:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "rsg" {
-  name     = rgtest
-  location = var.location
+  name     = "rgtest"
+  location = "North Europe"
   	tags = {
-		component = rgtest
+		component = "rgtest"
   }
 }
 
